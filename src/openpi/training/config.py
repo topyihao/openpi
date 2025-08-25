f9afecf7b3424d825029356b5304bd45813ed43a
Yihao Zhang <topyihao@gmail.com>
2025-08-26 00:29:06 +1000

    update

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -434,7 +434,7 @@ class TrainConfig:
     # How often (in steps) to log training metrics.
     log_interval: int = 100
     # How often (in steps) to save checkpoints.
-    save_interval: int = 2000
+    save_interval: int = 3000
     # If set, any existing checkpoints matching step % keep_period == 0 will not be deleted.
     keep_period: int | None = 5000
 
